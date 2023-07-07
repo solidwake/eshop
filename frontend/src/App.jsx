18ad8586970cb8b358b4ef1546dbd972e01e593d
Idris Westbrook <69250073+solidwake@users.noreply.github.com>
2023-07-07 16:46:19 -0400

    fix alignment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
 
 function App() {
   return (
-    <h1>Welcome to PurpleUsed</h1>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to PurpleUsed</h1>
+        </Container>
+      </main>
+    </>
   )
 }
 
