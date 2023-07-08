c629543084794df23705ac48f08f3337226b4a03
Idris Westbrook <69250073+solidwake@users.noreply.github.com>
2023-07-08 15:32:32 -0400

    add react-router-dom

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' elements={<App />}>
+
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
