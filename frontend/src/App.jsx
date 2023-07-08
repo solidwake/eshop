ba1153db184edb43d3d4948123bb873a2c0ced4d
Idris Westbrook <69250073+solidwake@users.noreply.github.com>
2023-07-08 00:12:45 -0400

    add footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <h1>Welcome to PurpleUsed</h1>
         </Container>
       </main>
+      <Footer />
     </>
   )
 }
