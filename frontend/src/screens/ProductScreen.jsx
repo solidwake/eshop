472e465389ae910ebb12b7c71e1291719b75b297
Idris Westbrook <69250073+solidwake@users.noreply.github.com>
2023-07-15 15:55:19 -0400

    add get product details screen base

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -4,6 +4,7 @@ import products from '../products'
 function ProductScreen() {
   const { id: productId } = useParams()
   const product = products.find((p) => p._id === productId)
+  console.log(product)
 
   return (
     <div>ProductScreen</div>
