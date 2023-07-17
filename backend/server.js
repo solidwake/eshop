d39f05668b69c87152ef6ebcaf8bf9cec4dd8698
Idris Westbrook <69250073+solidwake@users.noreply.github.com>
2023-07-16 20:05:56 -0400

    add backend api

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from 'express'
+import products from './data/products.js'
 const port = 5000
 
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('API is running...')
+})
+
+app.get('/data/products', (req, res) => {
+  res.json(products)
 })
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
