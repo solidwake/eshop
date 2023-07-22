bb64d04ecc5dc93a0345c391b47a25fdadd635e2
Idris Westbrook <69250073+solidwake@users.noreply.github.com>
2023-07-21 23:14:29 -0400

    connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
+import connectDB from './config/db.js'
 import products from './data/products.js'
 const port = process.env.PORT || 5000
+
+connectDB()
 
 const app = express()
 
