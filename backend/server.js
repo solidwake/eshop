0eff6803f95e701f41a4c8b34e3931980f5bee82
Idris Westbrook <69250073+solidwake@users.noreply.github.com>
2023-07-23 08:28:01 -0400

    add product routes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
 const port = process.env.PORT || 5000
 
 connectDB()
@@ -16,13 +16,6 @@ app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
-app.get('/data/products', (req, res) => {
-  res.json(products)
-})
-
-app.get('/data/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+app.use('/data/products', productRoutes)
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
