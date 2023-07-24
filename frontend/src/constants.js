17a247fade77a70c4af99f10fa0a08d8bfe1981e
Idris Westbrook <69250073+solidwake@users.noreply.github.com>
2023-07-23 22:14:35 -0400

    add react-redux

---
+++ frontend/src/constants.js
@@ -0,0 +1,6 @@
+export const BASE_URL = import.meta.env.NODE_ENV === 'development' ? 'http://localhost:5000' : ''
+
+export const PRODUCTS_URL = '/data/products'
+export const USERS_URL = '/data/users'
+export const ORDERS_URL = '/data/orders'
+export const PAYPAL_URL = '/data/config/paypal'
