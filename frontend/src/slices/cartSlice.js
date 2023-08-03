5b7b82295a7da873604ac111c8e1d6be3bc9f8a9
Idris Westbrook <69250073+solidwake@users.noreply.github.com>
2023-08-02 23:11:02 -0400

    add quantity to cart

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,10 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { updateCart } from '../utils/cartUtils'
 
 const initialState = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : { cartItems: [] }
-
-const addDecimals = (num) => {
-  return (Math.round(num * 100) / 100).toFixed(2)
-}
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -15,26 +12,13 @@ const cartSlice = createSlice({
 
       const existItem = state.cartItems.find((x) => x._id === item._id)
 
-      if(existItem) {
+      if (existItem) {
         state.cartItems = state.cartItems.map((x) => x._id === existItem._id ? item : x)
       } else {
         state.cartItems = [...state.cartItems, item]
       }
-      // Calculate items price
-      state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price = item.qty, 0))
-      // Calculate shipping price
-      state.shippingPrice = addDecimals(10)
-      // Calculate tax price
-      state.taxPrice = addDecimals(.05 * state.itemsPrice)
-      // Calculate total price
-      state.totalPrice = (
-        Number(state.itemsPrice) +
-        Number(state.shippingPrice) +
-        Number(state.taxPrice)
-        
-      ).toFixed(2)
 
-      localStorage.setItem('cart', JSON.stringify(state))
+      return updateCart(state)
     }
   }
 })
