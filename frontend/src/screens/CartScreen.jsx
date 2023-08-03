52953a3b2d29b679d87c7171084007e464afbdaa
Idris Westbrook <69250073+solidwake@users.noreply.github.com>
2023-08-03 10:47:45 -0400

    add cart screen boilerplate

---
+++ frontend/src/screens/CartScreen.jsx
@@ -0,0 +1,21 @@
+import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card
+} from 'react-bootstrap'
+import { FaTrash } from 'react-icons/fa'
+import Message from '../components/Message'
+
+export default function CartScreen() {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  return (
+    <div>CartScreen</div>
+  )
+}
