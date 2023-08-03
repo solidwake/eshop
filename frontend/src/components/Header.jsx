2ece317825caed3e0e687305642d9bea0069da86
Idris Westbrook <69250073+solidwake@users.noreply.github.com>
2023-08-03 10:41:14 -0400

    add display number of items in cart

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,16 @@
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import {
+  Badge,
+  Navbar,
+  Nav,
+  Container
+} from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import { LinkContainer } from 'react-router-bootstrap'
+import { useSelector } from 'react-redux'
 
 function Header() {
+  const { cartItems } = useSelector((state) => state.cart)
+  console.log(cartItems)
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
@@ -14,7 +22,15 @@ function Header() {
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
               <LinkContainer to='/cart'>
-                <Nav.Link><FaShoppingCart /> Cart</Nav.Link>
+                <Nav.Link><FaShoppingCart /> Cart
+                  {
+                    cartItems.length > 0 && (
+                      <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                        {cartItems.reduce((a, c) => a + c.qty, 0)}
+                      </Badge>
+                    )
+                  }
+                </Nav.Link>
               </LinkContainer>
               <LinkContainer to='/login'>
                 <Nav.Link><FaUser /> Sign In</Nav.Link>
