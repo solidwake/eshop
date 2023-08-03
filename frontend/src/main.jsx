31303a00aa7c7d9d88a47c46dcbd1a8953e6b3ea
Idris Westbrook <69250073+solidwake@users.noreply.github.com>
2023-08-03 10:50:44 -0400

    add cart screen route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,12 +13,14 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import HomeScreen from './screens/HomeScreen.jsx'
 import ProductScreen from './screens/ProductScreen.jsx'
+import CartScreen from './screens/CartScreen.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
       <Route path='/product/:id' element={<ProductScreen />} />
+      <Route path='/cart' element={<CartScreen />} />
     </Route>
   )
 )
