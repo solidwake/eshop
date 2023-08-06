2650167410afd2042a7c2010dd27e6a0ae6011a4
Idris Westbrook <69250073+solidwake@users.noreply.github.com>
2023-08-06 16:10:54 -0400

    add user logout and remove jwt cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -47,7 +47,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /data/users/logout
 // @acces Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send('logout user')
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0)
+  })
+
+  res.status(200).json({ message: 'Logged out successfully' })
 })
 
 // @desc  Get user profile
