3c85ca6d2511f555505915f5afde5e82a512d3c9
Idris Westbrook <69250073+solidwake@users.noreply.github.com>
2023-08-06 08:18:29 -0400

    add validate email and password

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,10 @@ connectDB()
 
 const app = express()
 
+// Body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use(cors())
 
 app.get('/', (req, res) => {
